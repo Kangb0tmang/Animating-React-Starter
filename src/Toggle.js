2758b118c3ce8e9acea2bccfa589ac36e97c2bc8
Jon Kang <jon@primemotive.com.au>
2019-11-01 16:43:02 +1100

    Vid #10: Transition w Mounting & Unmounting

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useTransition, animated } from "react-spring";
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
-  const { color, y } = useSpring({
-    color: isToggled ? "dodgerblue" : "springgreen",
-    y: isToggled ? 0 : 1
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacty: 0 }
   });
 
   return (
     <div>
-      <animated.h1
-        style={{
-          transform: y
-            .interpolate({
-              range: [0, 0.25, 0.5, 0.75, 1],
-              output: [0, -25, -50, -100, -50]
-            })
-            .interpolate(y => `translate3d(0, ${y}px, 0)`),
-          color
-        }}
-      >
-        Hello
-      </animated.h1>
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.h1 key={key} style={props}>
+              Hello
+            </animated.h1>
+          )
+      )}
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
     </div>
   );
