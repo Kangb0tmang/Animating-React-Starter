cf99ce5624f72e38aaca4abc35d032ad4e72294b
Jon Kang <jon@primemotive.com.au>
2019-11-01 17:14:00 +1100

    Vid #11: Transition w Multiple Components

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,8 +2,25 @@ import React, { useState } from "react";
 import { useTransition, animated } from "react-spring";
 
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const transition = useTransition(isToggled, null, {
+  const [items, setItems] = useState([
+    {
+      letter: "Z",
+      key: 1
+    },
+    {
+      letter: "I",
+      key: 2
+    },
+    {
+      letter: "O",
+      key: 3
+    },
+    {
+      letter: "N",
+      key: 4
+    }
+  ]);
+  const transition = useTransition(items, item => item.key, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacty: 0 }
@@ -11,17 +28,43 @@ const Toggle = () => {
 
   return (
     <div>
-      {transition.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.h1 key={key} style={props}>
-              Hello
-            </animated.h1>
-          )
-      )}
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: "S",
+              key: 1
+            }
+          ])
+        }
+      >
+        Toggle
+      </button>
     </div>
   );
 };
 
 export default Toggle;
+
+// const [isToggled, setToggle] = useState(false);
+// const transition = useTransition(isToggled, null, {
+//   from: { opacity: 0, position: "absolute" },
+//   enter: { opacity: 1 },
+//   leave: { opacty: 0 }
+// });
+
+/* <div style={{ position: "relative" }}>
+  {transition.map(({ item, key, props }) =>
+    item ? (
+      <animated.h1 style={props}>Hello</animated.h1>
+    ) : (
+      <animated.h1 style={props}>World</animated.h1>
+    )
+  )}
+  <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+</div> */
