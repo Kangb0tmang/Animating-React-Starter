0b4f4ec26a8fada11d74e95b5d12b409da27e4cd
Jon Kang <jon@primemotive.com.au>
2019-11-19 14:50:51 +1100

    Vid #13: Modal transition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
-import Routes from "./Routes";
 import Nav from "./Nav";
+import Modal from "./Modal";
 import Checkout from "./Checkout";
 import logo from "./logo.svg";
 import "./App.css";
@@ -30,7 +30,7 @@ const App = () => {
         {/* <Nav style={navAnimation} /> */}
       </header>
       <main>
-        <Routes />
+        <Modal />
         <Checkout isOpen={isNavOpen} />
       </main>
     </animated.div>
